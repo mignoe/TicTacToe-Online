54d897aa4ea5e5ec68d7115f828d82282e2d51d9
João Juvino <joao.pedro.santos@ccc.ufcg.edu.br>
2023-12-07 17:49:27 -0300

    docs: add documentation for some function

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,36 @@
-from flask import Flask
+"""
+Módulo com rotas iniciais básicas
+"""
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-app = Flask(__name__)
-
 @app.route("/")
 def hello_world():
+    """
+    Rota que retorna uma mensagem de saudação.
+
+    :return: Mensagem de saudação em HTML.
+    """
     return "<p>Hello, World!</p>"
+
 
 @app.route("/login", methods=["POST"])
 def login():
+    """
+    Rota para processar solicitações de login.
+
+    :return: Mensagem indicando se o login foi bem-sucedido ou não.
+    """
     username = request.form.get("username")
     password = request.form.get("password")
 
     # Check if the username and password are valid
     if username == "admin" and password == "password":
         return "Login successful"
-    else:
-        return "Invalid username or password"
+    return "Invalid username or password"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
